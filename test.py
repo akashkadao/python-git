14e4f8aba9b18128fcdd8ebd94ac07b8b8b8618e
akashkadao <akashkadao@gmail.com>
2022-06-20 09:25:49 +0530

    my first python commit

---
+++ test.py
@@ -0,0 +1,17 @@
+from app import app
+
+def test1():
+    response = app.test_client().get("/")
+    assert response.status_code == 200
+    
+def test2():
+    response = app.test_client().get("/base")
+    assert response.status_code == 200
+    
+def test3():
+    response = app.test_client().get("/base")
+    assert b"My Friend List" in response.data
+    assert b"Added Friend" in response.data
+    assert b"krunal" in response.data
+    assert b"mohit" in response.data
+    
